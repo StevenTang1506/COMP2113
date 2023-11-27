b0e22bd702bb54d2f0786b54ce41393327260130
Steven.T <61890694+StevenTang1506@users.noreply.github.com>
2023-11-28 02:14:19 +0800

    Create main.cpp
    A main function of creating a Game object.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "Game.h"
+
+int main() {
+    // Prompt the user to input the maximum number of innings
+    int maxInnings;
+    std::cout << "Enter the maximum number of innings: ";
+    std::cin >> maxInnings;
+
+    // Prompt the user to choose whether they want to attack first
+    bool attackFirst;
+    std::string response;
+    std::cout << "Do you want to attack first? (yes/no): ";
+    std::cin >> response;
+    
+    // Check the response
+    if (response == "yes" || response == "Yes" || response == "YES") {
+        attackFirst = true;
+    } else if (response == "no" || response == "No" || response == "NO") {
+        attackFirst = false;
+    } else {
+        std::cout << "Invalid input. Defaulting to attacking first.\n";
+        attackFirst = true;
+    }
+
+    // Create a Game object with the user's input
+    Game game(maxInnings, attackFirst);
+
+    // Play the game
+    game.play();
+
+    return 0;
+}
