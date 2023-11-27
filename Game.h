f9bfb589cceb6dd4d3f377b441cded77d12f5c7f
Steven.T <61890694+StevenTang1506@users.noreply.github.com>
2023-11-28 02:31:17 +0800

    Update Game.h to handle error.
    Renamed the play() function.

--- Game.h
+++ Game.h
@@ -6,22 +6,28 @@
 #include <random>
 #include <iostream>
 
+// The Game class includes functionality to save and load game states, handle game events, and control the main game loop.
 class Game {
 public:
     Game(int maxInnings, bool attackFirst);
     void loadGame();
     void saveGame();
-    void gameLoop();
+    void play();
 private:
+    // Game state variables
     int innings;
     int maxInnings;
     int strikes;
     int runs;
     bool isOver;
     bool attackFirst;
+    // Random number generation related variables
+
     std::random_device rd;
     std::mt19937 gen;
     std::uniform_int_distribution<> dis;
+
+    // Private member functions for internal game logic
     int getRandomEvent();
     void checkGameOver();
     void handleEvent(int event);
