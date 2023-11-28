eb524a3d6a1d6e6869ea607345f675dfa6cefccf
Steven.T <61890694+StevenTang1506@users.noreply.github.com>
2023-11-29 00:55:57 +0800

    Update main.cpp
    Added main menu for saving and loading the game.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,101 @@
 #include "Game.h"
 
-int main() {
-    // Prompt the user to input the maximum number of innings
+void mainMenu(Game& game) {
     int maxInnings;
-    std::cout << "Enter the maximum number of innings: ";
-    while (!(std::cin >> maxInnings) || maxInnings <= 0) {
-        std::cout << "Invalid input. Please enter a positive number: ";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
-
-    // Prompt the user to choose whether they want to attack first
     bool attackFirst;
-    std::string response;
-    std::cout << "Do you want to attack first? (yes/no): ";
-    std::cin >> response;
-    
-    // Check the response
-    if (response == "yes" || response == "Yes" || response == "YES") {
-        attackFirst = true;
-    } else if (response == "no" || response == "No" || response == "NO") {
-        attackFirst = false;
-    } else {
-        std::cout << "Invalid input. Defaulting to attacking first.\n";
-        attackFirst = true;
+    bool notLoaded;
+    while (true) {
+        std::cout << "1. Play Game\n";
+        std::cout << "2. Settings\n";
+        std::cout << "Choose an option: ";
+
+        int option;
+        std::cin >> option;
+        std::string response;
+
+        switch (option) {
+            case 1: // Play the game
+                // Prompt the user to input the maximum number of innings
+                if (notLoaded) {
+                
+                    std::cout << "Enter the maximum number of innings: ";
+                    while (!(std::cin >> maxInnings) || maxInnings <= 0) {
+                        std::cout << "Invalid input. Please enter a positive number: ";
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    }
+                    
+                        // Prompt the user to choose whether they want to attack first
+                        std::cout << "Do you want to attack first? (yes/no): ";
+                        std::cin >> response;
+
+                        // Check the response
+                        
+                        if (response == "yes" || response == "Yes" || response == "YES") {
+                            attackFirst = true;
+                        } else if (response == "no" || response == "No" || response == "NO") {
+                            attackFirst = false;
+                        } else {
+                            std::cout << "Invalid input. Defaulting to attacking first.\n";
+                            attackFirst = true;
+                        }
+                        // Update the Game object with the user's input
+                        game.setMaxInnings(maxInnings);
+                        game.setAttackFirst(attackFirst);
+                }
+                game.play();
+                break;
+
+            case 2: // Access settings
+                while (true) {
+                    std::cout << "\nSettings:\n";
+                    std::cout << "1. Save Game\n";
+                    std::cout << "2. Load Game\n";
+                    std::cout << "3. Back to Main Menu\n";
+                    std::cout << "4. Quit Game\n";
+                    std::cout << "Choose an option: ";
+
+                    int settingsOption;
+                    std::cin >> settingsOption;
+                    
+                    switch (settingsOption) {
+                        case 1: // Save the game
+                            game.saveGame();
+                            std::cout << "Game saved successfully.\n";
+                            break;
+                        case 2: // Load the game
+                            game.loadGame();
+                            notLoaded = false;
+                            std::cout << "Game loaded successfully.\n";
+                            break;
+                        case 3: // Back to main menu
+                            return;
+                        case 4: // Quit the game
+                            std::cout << "Quitting the game...\n";
+                            game.setUserQuit(true);
+                            return;
+                        default:
+                            std::cout << "Invalid option. Please try again.\n";
+                    }
+                }
+            default:
+                std::cout << "Invalid option. Please try again.\n";
+        }
     }
+}
 
-    // Create a Game object with the user's input
-    Game game(maxInnings, attackFirst);
+int main() {
+    // Create a Game object with default values
+    Game game;
 
-    // Play the game
-    game.play();
+    // Call the main menu
+    mainMenu(game);
+
+    if (game.getUserQuit()) {
+        std::cout << "Thank you for playing. See you next time!\n";
+    } else {
+        mainMenu(game);
+    }
 
     return 0;
 }
